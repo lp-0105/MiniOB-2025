c401695f9425ee4a3c969a70d95326c9581e8b03
lp-0105 <1705982285@qq.com>
2025-11-14 23:16:49 +0800

    date-11.14-2

--- src/observer/common/type/date_type.cpp
+++ src/observer/common/type/date_type.cpp
@@ -15,10 +15,26 @@ RC DateType::set_value_from_str(Value &val, const std::string &data) const {
   if (sscanf(data.c_str(), "%d-%d-%d", &y, &m, &d) != 3) {
     return RC::SCHEMA_FIELD_TYPE_MISMATCH;
   }
-  if (y < 1970 || y > 2038 || m < 1 || m > 12 || d < 1 || d > 31) {
+  if (m < 1 || m > 12 || d < 1) {
     return RC::SCHEMA_FIELD_TYPE_MISMATCH;
   }
+  common::DateTime dt;
+  int max_day = dt.max_day_in_month_for(y, m);
+  if (d > max_day) {
+    return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+  }
+
   int julian_day = common::DateTime::julian_date(y, m, d);
+  // lower bound: 1970-01-01 (JULIAN_19700101)
+  if (julian_day < common::DateTime::JULIAN_19700101) {
+    return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+  }
+  // upper bound: 2038-01-19 (time_t overflow limit)
+  const int JULIAN_20380119 = common::DateTime::julian_date(2038, 1, 19);
+  if (julian_day > JULIAN_20380119) {
+    return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+  }
+
   val.set_int(julian_day - common::DateTime::JULIAN_19700101);
   return RC::SUCCESS;
 }

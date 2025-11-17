dbb8e5b4df8b65a990b89c7dcd390eb10293de72
lp-0105 <1705982285@qq.com>
2025-11-17 17:30:20 +0800

    join-tables-11.17-2

--- src/observer/common/type/char_type.cpp
+++ src/observer/common/type/char_type.cpp
@@ -15,9 +15,39 @@ See the Mulan PSL v2 for more details. */
 
 int CharType::compare(const Value &left, const Value &right) const
 {
-  ASSERT(left.attr_type() == AttrType::CHARS && right.attr_type() == AttrType::CHARS, "invalid type");
-  return common::compare_string(
-      (void *)left.value_.pointer_value_, left.length_, (void *)right.value_.pointer_value_, right.length_);
+  ASSERT(left.attr_type() == AttrType::CHARS, "left type is not CHARS");
+  
+  // 如果右边也是CHARS类型，直接比较字符串
+  if (right.attr_type() == AttrType::CHARS) {
+    return common::compare_string(
+        (void *)left.value_.pointer_value_, left.length_, (void *)right.value_.pointer_value_, right.length_);
+  }
+  
+  // 如果右边是数值类型，尝试将左边的字符串转换为数值进行比较
+  if (right.attr_type() == AttrType::INTS) {
+    try {
+      int left_val = stoi(string(left.value_.pointer_value_));
+      return common::compare_int((void *)&left_val, (void *)&right.value_.int_value_);
+    } catch (exception const &ex) {
+      LOG_TRACE("failed to convert string to int. s=%s, ex=%s", left.value_.pointer_value_, ex.what());
+      // 如果转换失败，认为字符串小于任何数值
+      return -1;
+    }
+  }
+  
+  if (right.attr_type() == AttrType::FLOATS) {
+    try {
+      float left_val = stof(string(left.value_.pointer_value_));
+      return common::compare_float((void *)&left_val, (void *)&right.value_.float_value_);
+    } catch (exception const &ex) {
+      LOG_TRACE("failed to convert string to float. s=%s, ex=%s", left.value_.pointer_value_, ex.what());
+      // 如果转换失败，认为字符串小于任何数值
+      return -1;
+    }
+  }
+  
+  // 其他类型不支持比较
+  return INT32_MAX;
 }
 
 RC CharType::set_value_from_str(Value &val, const string &data) const
--- src/observer/sql/operator/hash_join_physical_operator.cpp
+++ src/observer/sql/operator/hash_join_physical_operator.cpp
@@ -72,69 +72,88 @@ RC HashJoinPhysicalOperator::open(Trx *trx)
   
   LOG_INFO("Hash Join: left_table=%s, right_table=%s", left_table_name.c_str(), right_table_name.c_str());
   
-  // 构建哈希表
-  while ((rc = left_->next()) == RC::SUCCESS) {
-    left_tuple_ = left_->current_tuple();
-    if (left_tuple_ == nullptr) {
-      continue;
+  // 检查是否有等值连接条件，如果没有，则不使用哈希连接
+  bool has_equal_condition = false;
+  for (auto &predicate : predicates_) {
+    if (predicate->type() == ExprType::COMPARISON) {
+      auto *comp_expr = dynamic_cast<ComparisonExpr *>(predicate.get());
+      if (comp_expr && comp_expr->comp() == CompOp::EQUAL_TO) {
+        has_equal_condition = true;
+        break;
+      }
     }
+  }
+  
+  // 只有存在等值连接条件时才构建哈希表
+  if (has_equal_condition) {
+    // 构建哈希表
+    while ((rc = left_->next()) == RC::SUCCESS) {
+      left_tuple_ = left_->current_tuple();
+      if (left_tuple_ == nullptr) {
+        continue;
+      }
 
-    // 从JOIN条件中提取连接键
-    for (auto &predicate : predicates_) {
-      if (predicate->type() == ExprType::COMPARISON) {
-        auto *comp_expr = dynamic_cast<ComparisonExpr *>(predicate.get());
-        if (comp_expr && comp_expr->comp() == CompOp::EQUAL_TO) {
-          // 获取连接键值
-          Value join_key_value;
-          // 获取左右表达式
-          auto &left_expr = comp_expr->left();
-          auto &right_expr = comp_expr->right();
-          
-          // 确定哪个表达式是左表字段
-          Expression *left_field_expr = nullptr;
-          if (left_expr->type() == ExprType::FIELD) {
-            auto *field_expr = dynamic_cast<FieldExpr *>(left_expr.get());
-            if (field_expr && field_expr->field().table()) {
-              // 检查这个字段是否属于左表
-              if (left_table_name == field_expr->field().table_name()) {
-                left_field_expr = left_expr.get();
-                LOG_INFO("Hash Join: Found left table field: %s.%s", 
-                         field_expr->field().table_name(), field_expr->field().field_name());
+      // 从JOIN条件中提取连接键
+      for (auto &predicate : predicates_) {
+        if (predicate->type() == ExprType::COMPARISON) {
+          auto *comp_expr = dynamic_cast<ComparisonExpr *>(predicate.get());
+          if (comp_expr && comp_expr->comp() == CompOp::EQUAL_TO) {
+            // 获取连接键值
+            Value join_key_value;
+            // 获取左右表达式
+            auto &left_expr = comp_expr->left();
+            auto &right_expr = comp_expr->right();
+            
+            // 确定哪个表达式是左表字段
+            Expression *left_field_expr = nullptr;
+            if (left_expr->type() == ExprType::FIELD) {
+              auto *field_expr = dynamic_cast<FieldExpr *>(left_expr.get());
+              if (field_expr && field_expr->field().table()) {
+                // 检查这个字段是否属于左表
+                if (left_table_name == field_expr->field().table_name()) {
+                  left_field_expr = left_expr.get();
+                  LOG_INFO("Hash Join: Found left table field: %s.%s", 
+                           field_expr->field().table_name(), field_expr->field().field_name());
+                }
+              }
+            } else if (right_expr->type() == ExprType::FIELD) {
+              auto *field_expr = dynamic_cast<FieldExpr *>(right_expr.get());
+              if (field_expr && field_expr->field().table()) {
+                // 检查这个字段是否属于左表
+                if (left_table_name == field_expr->field().table_name()) {
+                  left_field_expr = right_expr.get();
+                  LOG_INFO("Hash Join: Found left table field: %s.%s", 
+                           field_expr->field().table_name(), field_expr->field().field_name());
+                }
               }
             }
-          } else if (right_expr->type() == ExprType::FIELD) {
-            auto *field_expr = dynamic_cast<FieldExpr *>(right_expr.get());
-            if (field_expr && field_expr->field().table()) {
-              // 检查这个字段是否属于左表
-              if (left_table_name == field_expr->field().table_name()) {
-                left_field_expr = right_expr.get();
-                LOG_INFO("Hash Join: Found left table field: %s.%s", 
-                         field_expr->field().table_name(), field_expr->field().field_name());
+            
+            if (left_field_expr) {
+              RC value_rc = left_field_expr->get_value(*left_tuple_, join_key_value);
+              if (value_rc == RC::SUCCESS) {
+                string key = join_key_value.to_string();
+                hash_table_[key].push_back(left_tuple_);
+                LOG_INFO("Hash Join: Added left tuple with key=%s to hash table", key.c_str());
+              } else {
+                LOG_WARN("Hash Join: Failed to get value from left tuple for key field");
               }
-            }
-          }
-          
-          if (left_field_expr) {
-            RC value_rc = left_field_expr->get_value(*left_tuple_, join_key_value);
-            if (value_rc == RC::SUCCESS) {
-              string key = join_key_value.to_string();
-              hash_table_[key].push_back(left_tuple_);
-              LOG_INFO("Hash Join: Added left tuple with key=%s to hash table", key.c_str());
-            } else {
-              LOG_WARN("Hash Join: Failed to get value from left tuple for key field");
             }
           }
         }
       }
     }
-  }
 
-  if (rc != RC::RECORD_EOF) {
-    LOG_WARN("failed to scan left table. rc=%s", strrc(rc));
-    return rc;
-  }
+    if (rc != RC::RECORD_EOF) {
+      LOG_WARN("failed to scan left table. rc=%s", strrc(rc));
+      return rc;
+    }
 
-  LOG_INFO("Hash Join: Built hash table with %zu entries", hash_table_.size());
+    LOG_INFO("Hash Join: Built hash table with %zu entries", hash_table_.size());
+  } else {
+    // 没有等值连接条件，不构建哈希表，使用嵌套循环连接
+    LOG_INFO("Hash Join: No equal condition found, using nested loop join");
+  }
+  
   hash_built_ = true;
 
   // 打开右表（探测哈希表的一方）
@@ -158,53 +177,110 @@ RC HashJoinPhysicalOperator::next()
     return RC::INTERNAL;
   }
 
-  // 获取左表和右表的名称
-  string left_table_name;
-  string right_table_name;
+  RC rc = RC::SUCCESS;
   
-  // 获取左表名称
-  if (children_.size() >= 1 && children_[0]) {
-    auto *left_scan_oper = dynamic_cast<TableScanPhysicalOperator *>(children_[0].get());
-    if (left_scan_oper) {
-      left_table_name = left_scan_oper->param();
+  // 检查是否有等值连接条件
+  bool has_equal_condition = false;
+  for (auto &predicate : predicates_) {
+    if (predicate->type() == ExprType::COMPARISON) {
+      auto *comp_expr = dynamic_cast<ComparisonExpr *>(predicate.get());
+      if (comp_expr && comp_expr->comp() == CompOp::EQUAL_TO) {
+        has_equal_condition = true;
+        break;
+      }
     }
   }
   
-  // 获取右表名称
-  if (children_.size() >= 2 && children_[1]) {
-    auto *right_scan_oper = dynamic_cast<TableScanPhysicalOperator *>(children_[1].get());
-    if (right_scan_oper) {
-      right_table_name = right_scan_oper->param();
+  // 如果没有等值连接条件，使用嵌套循环连接
+  if (!has_equal_condition) {
+    // 使用嵌套循环连接处理非等值连接
+    while (true) {
+      // 如果当前还有匹配的左表元组，继续处理
+      if (current_tuple_index_ < current_right_tuples_.size()) {
+        // 创建复合元组
+        joined_tuple_.set_left(left_tuple_);
+        joined_tuple_.set_right(current_right_tuples_[current_tuple_index_]);
+        
+        // 评估所有连接条件
+        if (evaluate_join_conditions(joined_tuple_)) {
+          current_tuple_index_++;
+          return RC::SUCCESS;
+        }
+        current_tuple_index_++;
+      } else {
+        // 当前左表元组处理完毕，获取下一个左表元组
+        rc = left_->next();
+        if (rc != RC::SUCCESS) {
+          return rc;
+        }
+        
+        left_tuple_ = left_->current_tuple();
+        if (left_tuple_ == nullptr) {
+          continue;
+        }
+        
+        // 重置右表扫描
+        right_->close();
+        rc = right_->open(trx_);
+        if (rc != RC::SUCCESS) {
+          return rc;
+        }
+        
+        current_right_tuples_.clear();
+        current_tuple_index_ = 0;
+        
+        // 扫描右表，找到所有满足条件的元组
+        while ((rc = right_->next()) == RC::SUCCESS) {
+          right_tuple_ = right_->current_tuple();
+          if (right_tuple_ == nullptr) {
+            continue;
+          }
+          
+          // 创建临时复合元组进行条件评估
+          joined_tuple_.set_left(left_tuple_);
+          joined_tuple_.set_right(right_tuple_);
+          
+          // 评估所有连接条件
+          if (evaluate_join_conditions(joined_tuple_)) {
+            current_right_tuples_.push_back(right_tuple_);
+          }
+        }
+        
+        if (rc != RC::RECORD_EOF) {
+          return rc;
+        }
+      }
     }
   }
 
+  // 使用哈希连接处理等值连接
   while (true) {
-    // 如果当前还有匹配的左表元组需要处理
-    if (!current_right_tuples_.empty() && current_tuple_index_ < current_right_tuples_.size()) {
-      left_tuple_ = current_right_tuples_[current_tuple_index_];
-      joined_tuple_.set_left(left_tuple_);
+    // 如果当前还有匹配的左表元组，继续处理
+    if (current_tuple_index_ < current_right_tuples_.size()) {
+      // 创建复合元组
+      joined_tuple_.set_left(current_right_tuples_[current_tuple_index_]);
       joined_tuple_.set_right(right_tuple_);
       
-      // 评估所有JOIN条件
-      if (evaluate_join_conditions()) {
+      // 评估所有连接条件
+      if (evaluate_join_conditions(joined_tuple_)) {
         current_tuple_index_++;
-        return RC::SUCCESS;  // 找到匹配的行
+        return RC::SUCCESS;
+      }
+      current_tuple_index_++;
+    } else {
+      // 当前右表元组处理完毕，获取下一个右表元组
+      rc = right_->next();
+      if (rc != RC::SUCCESS) {
+        return rc;
       }
       
-      // 条件不满足，继续下一个左表元组
-      current_tuple_index_++;
-      continue;
-    }
-
-    // 当前右表行的所有左表匹配元组处理完毕，获取下一个右表行
-    current_right_tuples_.clear();
-    current_tuple_index_ = 0;
-    
-    RC rc = right_->next();
-    if (rc == RC::SUCCESS) {
       right_tuple_ = right_->current_tuple();
+      if (right_tuple_ == nullptr) {
+        continue;
+      }
       
-      // 从JOIN条件中提取连接键
+      // 从JOIN条件中提取连接键值
+      bool found_match = false;
       for (auto &predicate : predicates_) {
         if (predicate->type() == ExprType::COMPARISON) {
           auto *comp_expr = dynamic_cast<ComparisonExpr *>(predicate.get());
@@ -221,6 +297,11 @@ RC HashJoinPhysicalOperator::next()
               auto *field_expr = dynamic_cast<FieldExpr *>(left_expr.get());
               if (field_expr && field_expr->field().table()) {
                 // 检查这个字段是否属于右表
+                string right_table_name;
+                auto *right_scan_oper = dynamic_cast<TableScanPhysicalOperator *>(children_[1].get());
+                if (right_scan_oper) {
+                  right_table_name = right_scan_oper->param();
+                }
                 if (right_table_name == field_expr->field().table_name()) {
                   right_field_expr = left_expr.get();
                   LOG_INFO("Hash Join: Found right table field: %s.%s", 
@@ -231,6 +312,11 @@ RC HashJoinPhysicalOperator::next()
               auto *field_expr = dynamic_cast<FieldExpr *>(right_expr.get());
               if (field_expr && field_expr->field().table()) {
                 // 检查这个字段是否属于右表
+                string right_table_name;
+                auto *right_scan_oper = dynamic_cast<TableScanPhysicalOperator *>(children_[1].get());
+                if (right_scan_oper) {
+                  right_table_name = right_scan_oper->param();
+                }
                 if (right_table_name == field_expr->field().table_name()) {
                   right_field_expr = right_expr.get();
                   LOG_INFO("Hash Join: Found right table field: %s.%s", 
@@ -243,14 +329,17 @@ RC HashJoinPhysicalOperator::next()
               RC value_rc = right_field_expr->get_value(*right_tuple_, join_key_value);
               if (value_rc == RC::SUCCESS) {
                 string key = join_key_value.to_string();
-                LOG_INFO("Hash Join: Looking for key=%s in hash table (size=%zu)", key.c_str(), hash_table_.size());
+                LOG_INFO("Hash Join: Looking for key=%s in hash table", key.c_str());
+                
+                // 在哈希表中查找匹配的左表元组
                 auto it = hash_table_.find(key);
                 if (it != hash_table_.end()) {
-                  LOG_INFO("Hash Join: Found %zu matching tuples for key=%s", it->second.size(), key.c_str());
-                  // 保存所有匹配的左表元组
                   current_right_tuples_ = it->second;
-                } else {
-                  LOG_INFO("Hash Join: No matching tuples found for key=%s", key.c_str());
+                  current_tuple_index_ = 0;
+                  found_match = true;
+                  LOG_INFO("Hash Join: Found %zu matching left tuples for key=%s", 
+                           current_right_tuples_.size(), key.c_str());
+                  break;
                 }
               } else {
                 LOG_WARN("Hash Join: Failed to get value from right tuple for key field");
@@ -260,16 +349,10 @@ RC HashJoinPhysicalOperator::next()
         }
       }
       
-      // 如果没有找到匹配，继续下一行右表
-      if (current_right_tuples_.empty()) {
-        continue;
+      if (!found_match) {
+        current_right_tuples_.clear();
+        current_tuple_index_ = 0;
       }
-    } else if (rc == RC::RECORD_EOF) {
-      // 右表扫描完毕
-      return RC::RECORD_EOF;
-    } else {
-      LOG_WARN("failed to get next tuple from right table. rc=%s", strrc(rc));
-      return rc;
     }
   }
 }
@@ -312,21 +395,15 @@ void HashJoinPhysicalOperator::set_predicates(vector<unique_ptr<Expression>> &&p
   }
 }
 
-bool HashJoinPhysicalOperator::evaluate_join_conditions()
+bool HashJoinPhysicalOperator::evaluate_join_conditions(JoinedTuple &joined_tuple)
 {
   if (predicates_.empty()) {
     return true;
   }
 
-  // 创建复合元组，包含左右两个元组
-  // 这样JOIN条件可以同时引用两个表的字段
-  JoinedTuple composite_tuple;
-  composite_tuple.set_left(left_tuple_);
-  composite_tuple.set_right(right_tuple_);
-
   for (auto &predicate : predicates_) {
     Value value;
-    RC rc = predicate->get_value(composite_tuple, value);
+    RC rc = predicate->get_value(joined_tuple, value);
     if (rc != RC::SUCCESS) {
       LOG_WARN("failed to evaluate join condition. rc=%s", strrc(rc));
       return false;
